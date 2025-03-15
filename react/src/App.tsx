7e69259b26ef89cff4c641f3b501897da267e977
ssim31 <126112836+ssim31@users.noreply.github.com>
2025-03-14 20:52:24 -0400

    Add logo and name to nav bar/header

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,14 @@
-// import { useState } from 'react'
-// import './App.css'
+//import React from 'react';
+import Header from './components/header';
 import Footer from './components/footer';
 
 function App() {
   return (
-    <Footer></Footer>
-  )
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ react/src/components/header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../assets/CarPals_Logo_And_Name.png';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="CarPals Logo" className="logo" />
+      </div>
+      <nav className="nav-links">
+        <a href="/">Home</a>
+        <a href="/rides">Find Rides</a>
+        <a href="/offer">Offer Ride</a>
+        <a href="/profile">Profile</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
