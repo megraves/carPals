24cd24317c847dc8d429e572fd422749a772647d
laurensheaa <las0709@gmail.com>
2025-03-31 19:02:34 -0400

    added navigation between pages in header

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import RoutesButton from "./components/RoutesButton/RoutesButton";
@@ -12,18 +12,51 @@ function App() {
   const [isModalOpen, setModalOpen] = useState(false);
   
   return (
-    <div className="App">
-      <Header />
-      <RoutesButton openModal={() => setModalOpen(true)} />
-      <RoutesModal
-        isOpen={isModalOpen}
-        closeModal={() => setModalOpen(false)}
-      />
-      <MyRidesButton />
-      <MyPalsButton />
-      <MapComponent />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        {/* homepage */}
+        <Route
+          path="/"
+          element={
+            <div className="App">
+            <Header />
+            <RoutesButton openModal={() => setModalOpen(true)} />
+            <RoutesModal
+              isOpen={isModalOpen}
+              closeModal={() => setModalOpen(false)}
+            />
+            <MyRidesButton />
+            <MyPalsButton />
+            <MapComponent />
+            <Footer />
+          </div>
+          }
+        />
+
+        {/* profile page */}
+        <Route 
+          path="/profile"
+          element={
+            <h1>profile page</h1>
+          }
+        />
+        {/* find rides page */}
+        <Route
+          path="/rides"
+          element={
+            <h1>find rides page</h1>
+          }
+        />
+        {/* offer rides page */}
+        <Route 
+          path="/offer"
+          element={
+            <h1>offer rides page</h1>
+          }
+        />
+      </Routes>
+    </Router>
+
   );
 }
 
