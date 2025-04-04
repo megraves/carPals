b0c5f7c243e5099c7c84a562e8f69c61483f1ca0
ssim31 <126112836+ssim31@users.noreply.github.com>
2025-04-04 01:52:08 -0400

    implemented issue 17 and added additional style and functionality to Plan a Ride page

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,4 @@
-import { JSX, useState } from "react";
+import React, { JSX, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
@@ -9,156 +9,174 @@ import MapComponent from "./components/MapComponent/MapComponent";
 import ProfileInfo from "./components/ProfileInfo/ProfileInfo";
 import ProfileRides from "./components/ProfileRides/ProfileRides";
 import DropDownModal from "./components/DropDownModal/DropDownModal";
+import OfferingRidesModal from "./components/OfferingRidesModal/OfferingRidesModal";
+
 
 function App() {
-
-  const [isModalOpen, setModalOpen] = useState(false);
-  const [modalMode, setModalMode] = useState<"find" | "offer" | null>(null);
-  const [DropModalContent, setDropModalContent] = useState("");
-
-  const [ridesOpen, setRidesOpen] = useState(false);
-  const [rideList, setRideList] = useState<JSX.Element[] | null>(null);
-  const rides = ["Ride 1", "Ride 2", "Ride 3"];
-
-  const [palsOpen, setPalsOpen] = useState(false);
-  const [palsList, setPalsList] = useState<JSX.Element[] | null>(null);
-  const pals = ["Edwin Tran", "Macy Graves", "Sofia Simonoff", "Lauren Shea"];
-
+ const [isModalOpen, setModalOpen] = useState(false);
+ const [modalMode, setModalMode] = useState<"find" | "offer" | null>(null);
+ const [DropModalContent, setDropModalContent] = useState("");
+ const [ridesOpen, setRidesOpen] = useState(false);
+ const [rideList, setRideList] = useState<JSX.Element[] | null>(null);
+ const rides = ["Ride 1", "Ride 2", "Ride 3"];
+ const [palsOpen, setPalsOpen] = useState(false);
+ const [palsList, setPalsList] = useState<JSX.Element[] | null>(null);
+ const pals = ["Edwin Tran", "Macy Graves", "Sofia Simonoff", "Lauren Shea"];
+ const [showPageMap, setShowPageMap] = useState(true);
+ const [showOfferingRidesModal, setShowOfferingRidesModal] = useState(false);
 
 
-
-  const [showPageMap, setShowPageMap] = useState(true);
-
-  const openFindModal = () => {
-    setModalMode("find");
-    setModalOpen(true);
-  };
-
-  const openOfferModal = () => {
-    setModalMode("offer");
-    setModalOpen(true);
-  };
+ const openFindModal = () => {
+   setModalMode("find");
+   setModalOpen(true);
+ };
 
 
-
-  const mapToList = (inputs: string[]) => {
-    return inputs.map((input, index) => (
-      <li key={index} className="input-item">
-        <button className="input-button" 
-        onClick={() => handleItemClick(input)}
-        >
-        {input}
-        </button>
-      </li>
-    ));
-  };
-
-  const dropDown = (inputs: string[], listSetter: React.Dispatch<React.SetStateAction<JSX.Element[] | null>>,  setOpen: React.Dispatch<React.SetStateAction<boolean>>) => {
-    setOpen(prev => !prev);
-    listSetter(prev => (prev ? null : mapToList(inputs))); 
-  }
-
-  const handleItemClick = (item: string) => {
-    setDropModalContent(`Information about ${item}`);
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-    setDropModalContent(""); // Clear modal content after closing
-  };
+ const openOfferModal = () => {
+   setShowOfferingRidesModal(true);
+   setShowPageMap(false);
+ };
 
 
-  return (
-    <Router>
-      <Routes>
-        {/* homepage */}
-        <Route
-          path="/"
-          element={
-            <div className="home-page">
-              <Header />
-              <div className="home-content">
-                <div className="ride-list-button">
-                  <ActionButton
-                    label="My Rides"
-                    action={() => dropDown(rides, setRideList, setRidesOpen)}
-                    id="my-rides"
-                  />
-                  {ridesOpen && <ul className="rides-dropdown">{rideList}</ul>}
-                </div>
-                <div className="pal-list-button">
-                  <ActionButton
-                    label="My Pals"
-                    action={() => dropDown(pals, setPalsList, setPalsOpen)}
-                    id="my-pals"
-                  />
-                  {palsOpen && <ul className="pals-dropdown">{palsList}</ul>}
-                </div>
-                <DropDownModal showModal={isModalOpen} closeModal={closeModal} content={DropModalContent} />
-                <div className="map-wrapper-home">
-                  <MapComponent mapId="home-map" className="home-container" />
-                </div>
-              </div>
-              <Footer />
-            </div>
-          }
-        />
+ const mapToList = (inputs: string[]) => {
+   return inputs.map((input, index) => (
+     <li key={index} className="input-item">
+       <button className="input-button" onClick={() => handleItemClick(input)}>
+         {input}
+       </button>
+     </li>
+   ));
+ };
 
-        {/* profile page */}
-        <Route
-          path="/profile"
-          element={
-            <div className="profile-page">
-              <Header />
-              <ProfileInfo />
-              <ProfileRides />
-              <Footer />
-            </div>
-          }
-        />
 
-        {/* plan a ride page */}
-        <Route
-          path="/plan-rides"
-          element={
-            <div className="plan-rides">
-              <Header />
-              <div className="plan-rides-content">
-                <div className="button-section">
-                  <ActionButton
-                    label="Find a Ride"
-                    action={openFindModal}
-                    id="find-a-ride"
-                  />
-                  <ActionButton
-                    label="Offer a Ride"
-                    action={openOfferModal}
-                    id="offer-a-ride"
-                  />
-                </div>
-                <div className="map-wrapper-plan">
-                  {showPageMap && (
-                    <MapComponent mapId="plan-map" className="plan-container" />
-                  )}
-                </div>
-                <RoutesModal
-                  isOpen={isModalOpen}
-                  closeModal={() => {
-                    setModalOpen(false);
-                    setShowPageMap(true);
-                  }}
-                  mode={modalMode}
-                  setShowPageMap={setShowPageMap}
-                />
-              </div>
-              <Footer />
-            </div>
-          }
-        />
-      </Routes>
-    </Router>
-  );
+ const dropDown = (
+   inputs: string[],
+   listSetter: React.Dispatch<React.SetStateAction<JSX.Element[] | null>>,
+   setOpen: React.Dispatch<React.SetStateAction<boolean>>
+ ) => {
+   setOpen((prev) => !prev);
+   listSetter((prev) => (prev ? null : mapToList(inputs)));
+ };
+
+
+ const handleItemClick = (item: string) => {
+   setDropModalContent(`Information about ${item}`);
+   setModalOpen(true);
+ };
+
+
+ const closeModal = () => {
+   setModalOpen(false);
+   setDropModalContent("");
+ };
+
+
+ return (
+   <Router>
+     <Routes>
+       <Route
+         path="/"
+         element={
+           <div className="home-page">
+             <Header />
+             <div className="home-content">
+               <div className="ride-list-button">
+                 <ActionButton
+                   label="My Rides"
+                   action={() => dropDown(rides, setRideList, setRidesOpen)}
+                   id="my-rides"
+                 />
+                 {ridesOpen && <ul className="rides-dropdown">{rideList}</ul>}
+               </div>
+               <div className="pal-list-button">
+                 <ActionButton
+                   label="My Pals"
+                   action={() => dropDown(pals, setPalsList, setPalsOpen)}
+                   id="my-pals"
+                 />
+                 {palsOpen && <ul className="pals-dropdown">{palsList}</ul>}
+               </div>
+               <DropDownModal
+                 showModal={isModalOpen}
+                 closeModal={closeModal}
+                 content={DropModalContent}
+               />
+               <div className="map-wrapper-home">
+                 <MapComponent mapId="home-map" className="home-container" />
+               </div>
+             </div>
+             <Footer />
+           </div>
+         }
+       />
+
+
+       <Route
+         path="/profile"
+         element={
+           <div className="profile-page">
+             <Header />
+             <ProfileInfo />
+             <ProfileRides />
+             <Footer />
+           </div>
+         }
+       />
+
+
+       <Route
+         path="/plan-rides"
+         element={
+           <div className="plan-rides">
+             <Header />
+             <div className="plan-rides-content">
+               <div className="button-section">
+                 <ActionButton
+                   label="Find a Ride"
+                   action={openFindModal}
+                   id="find-a-ride"
+                 />
+                 <ActionButton
+                   label="Offer a Ride"
+                   action={openOfferModal}
+                   id="offer-a-ride"
+                 />
+               </div>
+               <div className="map-wrapper-plan">
+                 {showPageMap && (
+                   <MapComponent mapId="plan-map" className="plan-container" />
+                 )}
+               </div>
+               <RoutesModal
+                 isOpen={isModalOpen}
+                 closeModal={() => {
+                   setModalOpen(false);
+                   setShowPageMap(true);
+                 }}
+                 mode={modalMode}
+                 setShowPageMap={setShowPageMap}
+               />
+               <OfferingRidesModal
+                 isOpen={showOfferingRidesModal}
+                 closeModal={() => {
+                   setShowOfferingRidesModal(false);
+                   setShowPageMap(true);
+                 }}
+                 restoreMap={() => setShowPageMap(true)}
+                 onSuccess={() => {
+                   setShowOfferingRidesModal(false);
+                   setShowPageMap(true);
+                 }}
+               />
+             </div>
+             <Footer />
+           </div>
+         }
+       />
+     </Routes>
+   </Router>
+ );
 }
+
 
 export default App;
--- react/src/components/FindingRidesModal/FindingRidesModal.tsx
+++ react/src/components/FindingRidesModal/FindingRidesModal.tsx
@@ -3,103 +3,301 @@ import MapComponent from "../MapComponent/MapComponent";
 import RideCard from "../rideCard/rideCard";
 import "./FindingRidesModal.css";
 
+
 export interface RideMatch {
-  firstName: string;
-  lastName: string;
-  pickUpLocation: string;
-  dropOffLocation: string;
-  pickUpTime: string;
-  daysNeeded: string[];
+ id: string;
+ firstName: string;
+ lastName: string;
+ pickUpLocation: string;
+ dropOffLocation: string;
+ pickUpTime: string;
+ daysNeeded: string[];
+ distance: number;
+ rating?: number;
+ vehicle?: string;
 }
+
+
+interface RawRideData {
+ id?: string;
+ firstName: string;
+ lastName: string;
+ pickUpLocation: string;
+ dropOffLocation: string;
+ pickUpTime: string;
+ daysNeeded?: string[];
+ rating?: number;
+ vehicle?: string;
+}
+
 
 interface MatchingRidesModalProps {
-  isOpen: boolean;
-  closeModal: () => void;
-  onConfirm: (ride: RideMatch) => void;
-  restoreMap: () => void;
+ isOpen: boolean;
+ closeModal: () => void;
+ onConfirm: (ride: RideMatch) => void;
+ restoreMap: () => void;
 }
 
+
 const MatchingRidesModal: React.FC<MatchingRidesModalProps> = ({
-  isOpen,
-  closeModal,
-  onConfirm,
-  restoreMap,
+ isOpen,
+ closeModal,
+ restoreMap,
+ onConfirm
 }) => {
-  const [selectedRide, setSelectedRide] = useState<RideMatch | null>(null);
-  const [matchedRides, setMatchedRides] = useState<RideMatch[]>([]);
+ const [selectedRide, setSelectedRide] = useState<RideMatch | null>(null);
+ const [matchedRides, setMatchedRides] = useState<RideMatch[]>([]);
+ const [allRides, setAllRides] = useState<RideMatch[]>([]);
+ const [showConfirmation, setShowConfirmation] = useState(false);
+ const [showSuccess, setShowSuccess] = useState(false);
 
-  useEffect(() => {
-    if (isOpen) {
-      fetch("./mock_data.json")
-        .then((res) => res.json())
-        .then((data) => setMatchedRides(data.slice(1, 16)))
-        .catch((err) => console.error("Failed to fetch mock rides: ", err));
-    }
-  }, [isOpen]);
 
-  if (!isOpen) return null;
+ // Filter states
+ const [distanceFilter, setDistanceFilter] = useState<string>("");
+ const [driverNameFilter, setDriverNameFilter] = useState<string>("");
+ const [timeRangeFilter, setTimeRangeFilter] = useState<string>("");
 
-  const handleCloseModal = () => {
-    setSelectedRide(null);
-    restoreMap();
-    closeModal();
-  };
 
-  return (
-    <div className="modal-overlay">
-      <div className="modal-large">
-        <button className="close-button" onClick={handleCloseModal}>
-          &times;
-        </button>
+ useEffect(() => {
+   if (isOpen) {
+     const fetchMockData = async (): Promise<RideMatch[]> => {
+       try {
+         const response = await fetch("./mock_data.json");
+         const data: RawRideData[] = await response.json();
+         return data.slice(1, 16).map((ride: RawRideData) => ({
+           firstName: ride.firstName,
+           lastName: ride.lastName,
+           pickUpLocation: ride.pickUpLocation,
+           dropOffLocation: ride.dropOffLocation,
+           pickUpTime: ride.pickUpTime,
+           daysNeeded: ride.daysNeeded || [],
+           distance: Math.floor(Math.random() * 30) + 1,
+           id: ride.id || Math.random().toString(36).substring(2, 9),
+           rating: ride.rating,
+           vehicle: ride.vehicle
+         }));
+       } catch (error) {
+         console.error("Failed to fetch mock rides: ", error);
+         return [];
+       }
+     };
 
-        <div className="modal-layout">
-          {/* Left: Map */}
-          <div className="modal-map">
-            <MapComponent mapId="modal-map" className="modal-container" />
-            <button className="back-button" onClick={handleCloseModal}>
-              Back
-            </button>
-          </div>
 
-          {/* Right: Scrollable List with Fixed Submit Button */}
-          <div className="modal-list">
-            <h2 className="title">Nearby Drivers</h2>
-            <div className="ride-list-scroll">
-              {matchedRides.map((ride, index) => (
-                <div
-                  key={index}
-                  className={`ride-list-item ${
-                    selectedRide === ride ? "expanded" : ""
-                  }`}
-                  onClick={() => setSelectedRide(ride)}
-                >
-                  {selectedRide === ride ? (
-                    <RideCard {...ride} />
-                  ) : (
-                    <h3>
-                      {ride.firstName} {ride.lastName}
-                    </h3>
-                  )}
-                </div>
-              ))}
-            </div>
+     fetchMockData().then(rides => {
+       setAllRides(rides);
+       setMatchedRides(rides);
+     });
+   }
+ }, [isOpen]);
 
-            <div className="confirm-container">
-              <button
-                className="confirm-ok-button"
-                onClick={() => {
-                  closeModal();
-                  restoreMap();
-                }}
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+
+ useEffect(() => {
+   const applyFilters = (rides: RideMatch[]): RideMatch[] => {
+     let filtered = [...rides];
+
+
+     if (distanceFilter) {
+       const maxDistance = parseInt(distanceFilter);
+       filtered = filtered.filter(ride => ride.distance <= maxDistance);
+     }
+
+
+     if (driverNameFilter) {
+       filtered = filtered.filter(ride =>
+         `${ride.firstName} ${ride.lastName}`.toLowerCase()
+           .includes(driverNameFilter.toLowerCase())
+       );
+     }
+
+
+     if (timeRangeFilter) {
+       const [startHour, endHour] = timeRangeFilter.split('-').map(Number);
+       filtered = filtered.filter(ride => {
+         const rideHour = parseInt(ride.pickUpTime.split(':')[0]);
+         return rideHour >= startHour && rideHour < endHour;
+       });
+     }
+
+
+     return filtered;
+   };
+
+
+   setMatchedRides(applyFilters(allRides));
+ }, [distanceFilter, driverNameFilter, timeRangeFilter, allRides]);
+
+
+ const handleConfirmRide = () => {
+   if (selectedRide) {
+     onConfirm(selectedRide);
+     setShowConfirmation(false);
+     setShowSuccess(true);
+     setTimeout(() => {
+       setShowSuccess(false);
+       closeModal();
+       restoreMap();
+     }, 3000);
+   }
+ };
+
+
+ if (!isOpen) return null;
+
+
+ return (
+   <div className="modal-overlay">
+     {/* Confirmation Dialog */}
+     {showConfirmation && (
+       <div className="confirmation-dialog">
+         <div className="confirmation-content">
+           <h3>Confirm Ride Request</h3>
+           <p>Request ride with {selectedRide?.firstName} {selectedRide?.lastName}?</p>
+           <div className="confirmation-buttons">
+             <button
+               className="confirm-button"
+               onClick={handleConfirmRide}
+             >
+               Confirm
+             </button>
+             <button
+               className="cancel-button"
+               onClick={() => setShowConfirmation(false)}
+             >
+               Cancel
+             </button>
+           </div>
+         </div>
+       </div>
+     )}
+
+
+     {/* Success Message */}
+     {showSuccess && (
+       <div className="success-notification">
+         <svg viewBox="0 0 24 24" className="success-icon">
+           <path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z" />
+         </svg>
+         <span>Ride Requested Successfully!</span>
+       </div>
+     )}
+
+
+     <div className="modal-large">
+       <button
+         className="close-button"
+         onClick={closeModal}
+         aria-label="Close modal"
+       >
+         &times;
+       </button>
+
+
+       <div className="modal-layout">
+         <div className="modal-map">
+           <MapComponent mapId="modal-map" className="modal-container" />
+           <button className="back-button" onClick={closeModal}>
+             Back
+           </button>
+         </div>
+
+
+         <div className="modal-list">
+           <h2 className="title">Nearby Drivers</h2>
+
+
+           <div className="filter-section">
+             <div className="filter-group">
+               <label htmlFor="distance-filter">Distance:</label>
+               <select
+                 id="distance-filter"
+                 value={distanceFilter}
+                 onChange={(e) => setDistanceFilter(e.target.value)}
+               >
+                 <option value="">Any distance</option>
+                 <option value="5">Within 5 miles</option>
+                 <option value="10">Within 10 miles</option>
+                 <option value="25">Within 25 miles</option>
+               </select>
+             </div>
+
+
+             <div className="filter-group">
+               <label htmlFor="driver-filter">Driver Name:</label>
+               <input
+                 id="driver-filter"
+                 type="text"
+                 placeholder="Search driver..."
+                 value={driverNameFilter}
+                 onChange={(e) => setDriverNameFilter(e.target.value)}
+               />
+             </div>
+
+
+             <div className="filter-group">
+               <label htmlFor="time-filter">Departure Time:</label>
+               <select
+                 id="time-filter"
+                 value={timeRangeFilter}
+                 onChange={(e) => setTimeRangeFilter(e.target.value)}
+               >
+                 <option value="">Any time</option>
+                 <option value="0-6">12am - 6am</option>
+                 <option value="6-12">6am - 12pm</option>
+                 <option value="12-18">12pm - 6pm</option>
+                 <option value="18-24">6pm - 12am</option>
+               </select>
+             </div>
+           </div>
+
+
+           <div className="ride-list-scroll">
+             {matchedRides.length > 0 ? (
+               matchedRides.map((ride: RideMatch) => (
+                 <div
+                   key={ride.id}
+                   className={`ride-list-item ${
+                     selectedRide?.id === ride.id ? "expanded" : ""
+                   }`}
+                   onClick={() => {
+                     setSelectedRide(ride);
+                     setShowConfirmation(true);
+                   }}
+                 >
+                   {selectedRide?.id === ride.id ? (
+                     <RideCard {...ride} />
+                   ) : (
+                     <div className="ride-summary">
+                       <h3>{ride.firstName} {ride.lastName}</h3>
+                       <p>{ride.distance} miles away</p>
+                       <p>Departure: {ride.pickUpTime}</p>
+                     </div>
+                   )}
+                 </div>
+               ))
+             ) : (
+               <div className="no-results">
+                 No rides match your filters. Try adjusting your criteria.
+               </div>
+             )}
+           </div>
+
+
+           <div className="confirm-container">
+             <button
+               className="modern-button"
+               onClick={() => {
+                 closeModal();
+                 restoreMap();
+               }}
+             >
+             </button>
+           </div>
+         </div>
+       </div>
+     </div>
+   </div>
+ );
 };
+
 
 export default MatchingRidesModal;
